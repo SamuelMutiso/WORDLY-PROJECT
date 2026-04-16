7bafcc6234e344b44aa12ffe8b755917df2c63f6
SamuelMutiso <muuosamuel335@gmail.com>
2026-04-16 20:16:39 +0300

    created a function that talks to the API

--- main.js
+++ main.js
@@ -17,3 +17,25 @@ myForm.addEventListener("submit", function(event){
     findWord(userWord);
 })
 
+// we need to get a function that talks the API
+function findWord(word) {
+    fetch(apiBase + word)
+        .then(function(response){
+            //we need to check if the word typed actually exists
+            if(response.ok) {
+                return response.json(); //promise 
+            } else {
+                //if the input word is not found it should display and error
+                throw new Error("word not found");
+            }
+
+        })
+        .then(function(data) {
+            // send like a display (success!) msg
+            displayResult(data[0]);
+        })
+        .catch(function(error) {
+            // ifthe user finds an error this message should be displayed 
+            displayBox.innerHTML = "<p class= 'error-msg'>No results found.</p>"
+        })
+}
